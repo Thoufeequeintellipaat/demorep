a54903712ade556cc09464d5a946ef5bffa8f63e
johnabelardom <abelardomanangan@gmail.com>
2016-07-07 00:08:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
-<!DOCTYPE>
-<html>
 
-  <head>
-    <title>   </title>
-  </head>
-  
-  <body>
-  
-  
-  
-    <header class="header">
-      <h1 class="logo">SOME LOGO HERE</h1>
-      <ul class="nav-list">
-        <li>Sample 1</li>
-        <li>Sample 2</li>
-        <li>Sample 3</li>
-      </ul>
-    </header>
-    
-    
-    
-    <section class="section">
       <div class="primary-content">
         <div class="form-div">
           <form method="post">
