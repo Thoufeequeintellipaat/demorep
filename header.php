a2d0908c7e4f7044e9d047ea26a7d2866ffcea4e
johnabelardom <abelardomanangan@gmail.com>
2016-07-07 00:08:52 +0800

    original header
    created header

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE>
+<html>
+
+  <head>
+    <title>   </title>
+  </head>
+  
+  <body>
+  
+  
+  
+    <header class="header">
+      <h1 class="logo">SOME LOGO HERE</h1>
+      <ul class="nav-list">
+        <li>Sample 1</li>
+        <li>Sample 2</li>
+        <li>Sample 3</li>
+      </ul>
+    </header>
+    
+    
+    
+    <section class="section">
