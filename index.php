7db3afcfe37400df40090bb05033e90f43110cca
johnabelardom <abelardomanangan@gmail.com>
2016-07-07 00:07:20 +0800

    original index
    created index file

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE>
+<html>
+
+  <head>
+    <title>   </title>
+  </head>
+  
+  <body>
+  
+  
+  
+    <header class="header">
+      <h1 class="logo">SOME LOGO HERE</h1>
+      <ul class="nav-list">
+        <li>Sample 1</li>
+        <li>Sample 2</li>
+        <li>Sample 3</li>
+      </ul>
+    </header>
+    
+    
+    
+    <section class="section">
+      <div class="primary-content">
+        <div class="form-div">
+          <form method="post">
+            <input type="text" name="username" placeholder="Username" id="username" class="inputs">
+            <input type="text" name="username" placeholder="Username" id="username" class="inputs">
+            <input type="submit" value="Submit" onclick="checkfields()">
+          </form>
+        </div
+      </div>
+    </section>
+    
+    
+    
+    <footer class="footer">
+      <?php echo "&copy;" . date('Y') . ". Sample Website"; ?>
+    </footer>
+  
+  </body>
+
+</html>
