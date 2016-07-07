34c0ad22b5545ea571d2d11132bde78c3aa6b140
johnabelardom <abelardomanangan@gmail.com>
2016-07-07 23:18:47 +0800

    index update 2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <div class="form-div">
           <form method="post">
             <input type="text" name="username" placeholder="Username" id="username" class="inputs">
-            <input type="password" name="password" placeholder="Username" id="password" class="inputs">
+            <input type="password" name="password" placeholder="Password" id="password" class="inputs">
             <input type="submit" value="Submit" onclick="checkfields()">
           </form>
         </div
